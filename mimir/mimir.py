0ae50b3ef3cad998baca7bcb3b1bf1224a98b056
Erencan Ekinci <kyse1234@hotmail.com>
2025-01-30 20:34:07 +0300

    add: mimir handlers

--- mimir/mimir.py
+++ mimir/mimir.py
@@ -4,3 +4,10 @@ from telegram.ext import CommandHandler, CallbackQueryHandler, Application, Cont
 async def start(update: Update, context: CallbackQueryHandler) -> None:
 
     await update.message.reply_text("Step forth, seeker—if wisdom is your mead, drink deep, but know its cost.")
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(
+        """I heed these summons:\n/start – The journey begins, the path unwinds.
+        \n/help – Seek guidance, and I shall whisper the way.
+        """
+    )
