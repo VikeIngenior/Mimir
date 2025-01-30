4b4f522271377a871367c6773f7847655863b4e6
Erencan Ekinci <kyse1234@hotmail.com>
2025-01-30 21:17:30 +0300

    add: connect agent to Mimir

--- mimir/mimir.py
+++ mimir/mimir.py
@@ -1,11 +1,17 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, CallbackQueryHandler, Application, ContextTypes, MessageHandler, Updater, filters
+from urllib3.response import HAS_ZSTD
+
+from ai_agent.mimir_agent import MyAIAgent
+
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
 
 API_TOKEN = os.environ['API_TOKEN']
+agent = MyAIAgent()
 
 async def start(update: Update, context: CallbackQueryHandler) -> None:
 
@@ -18,10 +24,19 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         """
     )
 
+async def handle_message(update: Update, context) -> None:
+    user_message = update.message.text
+    bot_response = agent.get_response(user_message)
+    await update.message.reply_text(bot_response)
+
+
 def runner():
     application = Application.builder().token(API_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help))
+
+    message_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
+    application.add_handler(message_handler)
 
     application.run_polling()
